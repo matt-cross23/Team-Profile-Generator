24f2943f4978e7ad15fe2dc14cc484badf72e873
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2021-11-10 10:38:55 -0500

    Update employee.test.js

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,23 +1,36 @@
 const Employee = require('./lib/Employee');
 
-describe('Employee Object', () => {
-it('Should have name, id, and email in the object', () =>{
-const employee = new Employee('Matthew', 90, "matthewcross1017@gmail.com");
+describe('Returns Employee Object', () => {
+it('Should have name, id, and email in the object', () => {
+const employee = new Employee('Matthew', 67, "matthewcross1017@gmail.com");
 expect(employee.name).toBe(String);
 expect(employee.id).toBe(Number);
 expect(employee.email).toBe(String);
+expect(typeof(employee)).toEqual("object")
 });
+
 describe('name'), () => {
 it('Should return name of employee')
 const name = "Matthew"
-const person = new Employee(name)
-expect(person.getName()).toBe(name)
-}
+const employeeName = new Employee.getName()
+expect(employeeName).toBe(name)
+});
+    
 describe('id'), () =>{
     it ('Should return Id of employee')
     const testId = 30;
-    const empolyeeId = new Employee(id)
-    expect(empolyee.getId()).toBe(id)
-
-}
+    const empolyeeId = new Employee.getId()
+    expect(empolyeeId).toBe(testId)
 });
+describe('email'), () =>{
+ it ('Should return email of employee')
+    const testEmail = matthewcross1017@gmail.com 
+    const employeeEmail = new Employee.getEmail()
+    expect(employeeEmail).toBe(testEmail)
+});
+describe('role'), () =>{
+ it ('Should return employee as role when employee object is completed')
+ const testRole = "Employee"
+ const employeeRole = new Employee.getRole()
+ expect(employeeRole).toBe(testRole)
+}   
