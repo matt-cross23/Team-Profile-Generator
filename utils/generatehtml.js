df74c1777a2d9a79b2bb2e87d7bcf7ae97ebed00
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-02-03 13:38:44 -0500

    Add scorecards for positions

--- utils/generatehtml.js
+++ utils/generatehtml.js
@@ -1,3 +1,46 @@
+const generateHTML = (team) => {
+const createManager = (manager) => {
+return `
+<div class="card manager-card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Name: ${manager.getName()}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Role: ${manager.getRole()}</h6>
+    <p class="card-text">Find me at Office # ${manager.getOfficeNumber()}</p>
+    <p class="card-text">ID: ${manager.getID()}</p>
+    <a href="#${manager.getEmail()}" class="card-link">${manager.getEmail()}</a>
+  </div>
+</div>
+`;
+};
+const createEngineer = (engineer) => {
+return `
+<div class="card engineer-card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Name: ${engineer.getName()}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Role: ${engineer.getRole()}</h6>
+    <a href="https://github.com/${engineer.getGithub()}" class="card-link">${engineer.getGithub()}</a>
+    <p class="card-text">ID: ${engineer.getID()}</p>
+    <a href="${engineer.getEmail()}" class="card-link">${engineer.getEmail()}</a>
+  </div>
+</div>
+`;
+};
+
+  const createIntern = (intern) => {
+return `
+<div class="card intern-card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Name: ${intern.getName()}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Role: ${intern.getRole()}</h6>
+    <p class="card-text">I am atending ${intern.getSchool()}</p>
+    <p class="card-text">ID: ${intern.getID()}</p>
+    <a href="#{intern.getEmail()}" class="card-link">${intern.getEmail()}</a>
+  </div>
+</div>
+`;
+};  
+ 
+  
 
 function generateHTML({name, ID, email, github, linkedin}){
   return `
@@ -13,7 +56,7 @@ function generateHTML({name, ID, email, github, linkedin}){
   <div class="jumbotron jumbotron-fluid">
   <div class="container">
     <h1 class="display-4">Employee: ${name}</h1>
-    <p class="lead">ID ${ID}.</p>
+    <p class="lead">ID: ${ID}.</p>
     <h3>Manager<span class="badge badge-secondary">Contact Me</span></h3>
     <ul class="list-group">
     <li class="list-group-item">Their email is ${email}</li>
