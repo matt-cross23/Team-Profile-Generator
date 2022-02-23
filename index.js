3d43a9fb4ca5d8ca6f6400f44bc8dee61e2195d1
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-02-23 16:14:34 -0500

    Restart Inquirer Prompts and write to file func

--- index.js
+++ index.js
@@ -1,121 +1,213 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateHTML = require("./utils/generatehtml.js");
+
 const employees = [];
 
-const managerQuestions = [
+const managerQuestions = () => { 
+  return inquirer.prompt([
   {
     type: "input",
-    message: "What is your name?",
-    name: "name",
+    message: "What is the manager's name?",
+    name: "Name",
   },
   {
     type: "input",
-    message: "Employee ID?",
+    message: "Enter the manager's ID",
     name: "ID",
   },
   {
     type: "input",
-    message: "What is your email address",
-    name: "email",
+    message: "Enter your email address",
+    name: "Email",
   },
   {
     type: "input",
-    message: "Office number",
+    message: "Enter the Office number",
     name: "Office-Number",
   },
-];
-const startMenuQuestions = [
-  {
+])
+  .then(answers => {
+  const {Name, ID, Email, Office-Number} = answers
+  const newManager = new Manager( Name, ID, Email, Office-Number);
+    employees.push(newManager);
+    console.log(newManager);
+  };
+const startMenuQuestions () => {
+  console.log('Add more employees or finish!');
+    
+    return inquirer.prompt ([
     type: "list",
-    message: "Would you like to add a Team member?",
-    choices: ["engineer", "Intern", "No"],
-    name: "Menu",
+    message: "What's the empolyee's role?",
+    choices: ["engineer", "Intern"],
+    name: "Role",
   },
-];
-
-const engineerQuestions = [
-  {
+      
+       {
     type: "input",
-    message: "What is their name?",
-    name: "name",
+    message: "Enter the employee's name",
+    name: "Name",
   },
-  {
+      {
     type: "input",
-    message: "Employee ID?",
+    message: "Enter the employee's ID",
     name: "ID",
   },
-  {
+     {
     type: "input",
-    message: "What is your email address?",
-    name: "email",
+    message: "Enter them employee's email address",
+    name: "Email",
   },
-  {
+     {
     type: "input",
-    message: "What is your github",
-    name: "github",
+    when: (input) => input.role === "engineer"
+    message: "What's the employee's github?",
+    name: "Github",
   },
-];
-const internQuestions = [
-  {
+     {
     type: "input",
-    message: "What is your name?",
-    name: "name",
+    when: (input) => input.role === "intern"
+    message: "What's the enployee's school?",
+    name: "School",
   },
-  {
-    type: "input",
-    message: "Employee ID?",
-    name: "ID",
-  },
-  {
-    type: "input",
-    message: "What is your email address?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "What school did you attend",
-    name: "school",
-  },
-];
-function engineerPrompt() {
-  inquirer.prompt(engineerQuestions).then((answers) => {
-    employees.push(answers);
-    menuPrompt()
-  });
+    {
+      type: 'confirm',
+      name: 'endStartMenu'
+      message: 'Press Y to add more memebers',
+      default: false
+    }
+]);
+.then(employeeList) => {
+let {
+Role,
+Name,
+ID,
+Email,
+Github,
+School,
+endStartMenu;
+} = employeeList
+let newEmployee;
+    if ("role === engineer"){
+        new Employee = new Engineer (Name, ID, Email, Github)
+    console.log(newEmployee)
+    }
+    else if ("role === intern"){
+        new Employee = new Intern (Name, ID, Email, School)
+    console.log(newEmployee)
+    }
+  employees.push(newEmployee)
+if(endStartMenu) {
+  return newEmployee(employees)
+} else {
+  console.log(employees);
+  return employees
 }
-function internPrompt() {
-  inquirer.prompt(internQuestions).then((answers) => {
-    employees.push(answers);
-    menuPrompt()
-  });
-}
-function menuPrompt() {
-  inquirer.prompt(startMenuQuestions).then((answers) => {
-    console.log(answers)
-    switch (answers.Menu) {
-      case "engineer":
-        engineerPrompt();
-        break;
-      case "Intern":
-        internPrompt();
-        break;
-      default:
-        const generatedHTML = generateHTML(answers);
-        console.log(generatedHTML);
-    };
-  });
-}
-  
-function init() {
-  inquirer.prompt(managerQuestions).then((answers) => {
-    employees.push(answers);
-    menuPrompt();
-  fs.writeFileSync("./dist/scorecard.html", generateHTML(answers), function (err) {
+newManager()
+.then(newEmployee)
+ .then(employees => { 
+return generateHTML(employees);
+})
+.then(HTML => {
+  fs.writeFileSync("./dist/scorecard.html", generateHTML(HTML), function (err) {
      err
      ? console.log("error")
      : console.log("You've created the HTML file!");
     });
   });
-}
-init();
+  
+// const engineerQuestions = [
+//   {
+//     type: "input",
+//     message: "What is their name?",
+//     name: "name",
+//   },
+//   {
+//     type: "input",
+//     message: "Employee ID?",
+//     name: "ID",
+//   },
+//   {
+//     type: "input",
+//     message: "What is your email address?",
+//     name: "email",
+//   },
+//   {
+//     type: "input",
+//     message: "What is your github",
+//     name: "github",
+//   },
+// ];
+// const internQuestions = [
+//   {
+//     type: "input",
+//     message: "What is your name?",
+//     name: "name",
+//   },
+//   {
+//     type: "input",
+//     message: "Employee ID?",
+//     name: "ID",
+//   },
+//   {
+//     type: "input",
+//     message: "What is your email address?",
+//     name: "email",
+//   },
+//   {
+//     type: "input",
+//     message: "What school did you attend",
+//     name: "school",
+//   },
+  
+ 
+// ]);
+  
+  
+  
+  
+  
+// function engineerPrompt() {
+//   inquirer.prompt(engineerQuestions).then((answers) => {
+//     employees.push(answers);
+//     menuPrompt()
+//   });
+// }
+// function internPrompt() {
+//   inquirer.prompt(internQuestions).then((answers) => {
+//     employees.push(answers);
+//     menuPrompt()
+//   });
+// }
+// function menuPrompt() {
+//   inquirer.prompt(startMenuQuestions).then((answers) => {
+//     console.log(answers)
+//     switch (answers.Menu) {
+//       case "engineer":
+//         engineerPrompt();
+//         break;
+//       case "Intern":
+//         internPrompt();
+//         break;
+//       default:
+//         const generatedHTML = generateHTML(answers);
+//         console.log(generatedHTML);
+//     };
+//   });
+// }
+  
+// function init() {
+//   managerQuestions()
+//     .then((answers) 
+//     .then(teamArray ->{
+//     )      => {
+//     employees.push(answers);
+//     menuPrompt();
+//   fs.writeFileSync("./dist/scorecard.html", generateHTML(answers), function (err) {
+//      err
+//      ? console.log("error")
+//      : console.log("You've created the HTML file!");
+//     });
+//   });
+// }
+// init();
